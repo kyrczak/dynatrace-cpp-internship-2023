c4b5667baa430d656ef87069bcc3aa94454a8d18
kyrczak <patkor9@gmail.com>
2023-04-26 01:29:06 +0200

    Working on impelmentation of proper algorithm

--- project/src/internship.cpp
+++ project/src/internship.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 
 #include <date/date.h>
+#include <boost/date_time/gregorian/gregorian.hpp>
 #include <nlohmann/json.hpp>
 
 #include "internship.h"
 
 using json = nlohmann::json;
 using namespace date;
+using namespace boost::gregorian;
 
 namespace internship {
     // remove this function before submitting your solution
@@ -24,6 +26,28 @@ namespace internship {
         }
     }
 
+    void extractOS(const std::string& jsonFileName, int elementsCount) {
+        std::ifstream f(jsonFileName);
+        json data = json::parse(f);
+        int remainingElements = elementsCount;
+        date releaseDate;
+        date eolDate;
+        days versionLife(0);
+        days lastBestVerison(0);
+
+        for(const auto& [id,product] : data.items() {
+            if(product["os"] == true && remainingElements > 0) {
+                for (const auto& [id, version] : product["versions"].items()) {
+                    releaseDate = date(from_string(version["releaseDate"]));
+                    eolDate = date(from_string(version["eol"]));
+                    versionLife = eolDate - releaseDate;
+                    if(versionLife > lastBestVerison) {
+                        lastBestVerison = versionLife;
+                    }
+                }
+            }
+        })
+    }
     // do not remove this function
     void solution(const std::string& jsonFileName, int elementsCount) {
         example(jsonFileName); // remove this call before submitting your solution
